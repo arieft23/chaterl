1205d2fdc5cf3515e243ec04fdfe4412125c78af
arieft23 <arieftritomo@gmail.com>
2018-12-04 22:03:41 +0700

    implement case <<setname>> dan <<chat>> pada ws_handle

--- src/chat_cowboy_ws_handler.erl
+++ src/chat_cowboy_ws_handler.erl
@@ -1,5 +1,5 @@
 -module(chat_cowboy_ws_handler).
-
+-import(string,[concat/2]).
 -export([init/3]).
 -export([websocket_init/3]).
 -export([websocket_handle/3]).
@@ -9,7 +9,7 @@
 -define(CHATROOM_NAME, ?MODULE).
 -define(TIMEOUT, 5 * 60 * 1000). % Innactivity Timeout
 
--record(state, {name, handler}).
+-record(state, {name, username, handler}).
 
 %% API
 
@@ -20,16 +20,23 @@ websocket_init(_Type, Req, _Opts) ->
   % Create the handler from our custom callback
   Handler = ebus_proc:spawn_handler(fun chat_erlbus_handler:handle_msg/2, [self()]),
   ebus:sub(Handler, ?CHATROOM_NAME),
-  {ok, Req, #state{name = get_name(Req), handler = Handler}, ?TIMEOUT}.
+  {ok, Req, #state{name = get_name(Req), username= <<"default">> , handler = Handler}}.
 
 websocket_handle({text, Msg}, Req, State) ->
-  ebus:pub(?CHATROOM_NAME, {State#state.name, Msg}),
-  {ok, Req, State};
+  {ok, {Type, Msg1}} = parse_message(Msg),
+  case Type of
+      <<"setname">> ->
+        {ok, Req, NewState};
+      <<"chat">> ->
+        ebus:pub(?CHATROOM_NAME, {State#state.name, Msg1, State#state.username}),
+        {ok, Req, State}
+  end;
+  
 websocket_handle(_Data, Req, State) ->
   {ok, Req, State}.
 
-websocket_info({message_published, {Sender, Msg}}, Req, State) ->
-  {reply, {text, jiffy:encode({[{sender, Sender}, {msg, Msg}]})}, Req, State};
+websocket_info({message_published, {Sender, Msg, Username}}, Req, State) ->
+  {reply, {text, jiffy:encode({[{sender, Sender}, {username, Username}, {msg, Msg}]})}, Req, State};
 websocket_info(_Info, Req, State) ->
   {ok, Req, State}.
 
@@ -45,4 +52,9 @@ get_name(Req) ->
   Name = list_to_binary(string:join([inet_parse:ntoa(Host), 
     ":", io_lib:format("~p", [Port])], "")),
   Name.
-  
+
+parse_message(Msg) ->
+    {struct, Msg1}         = mochijson2:decode(Msg),
+    {<<"type">>, Type}     = lists:keyfind(<<"type">>, 1, Msg1),
+    {<<"msg">>,  Content}  = lists:keyfind(<<"msg">>, 1, Msg1),
+    {ok, {Type, Content}}.
